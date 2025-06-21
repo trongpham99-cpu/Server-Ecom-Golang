5464e35be14b5f497cd5490be647cbe7b25c86ab
trong.phamtranduc@gmail.com <trong.phamtranduc@gmail.com>
2025-06-21 11:03:37 +0700

    feat: crud API products

--- main.go
+++ main.go
@@ -1,17 +1,70 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	ID          uint    `json:"id" gorm:"primaryKey" gorm:"autoIncrement"`
+	Name        string  `json:"name" gorm:"size:255;not null"`
+	Price       float64 `json:"price" gorm:"not null"`
+	Description string  `json:"description" gorm:"type:text"`
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := gin.Default()
+
+	dsn := os.Getenv("DSN")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	fmt.Println("Connected to database successfully")
+
+	// db.AutoMigrate(&Product{})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.GET("/products", func(c *gin.Context) {
+		var products []Product
+		if err := db.Find(&products).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, products)
+	})
+
+	r.POST("/products", func(c *gin.Context) {
+		var product Product
+		if err := c.ShouldBindJSON(&product); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if err := db.Create(&product).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusCreated, product)
+	})
+
+	r.Run(":8080") // Run the server on port 8080
 }
